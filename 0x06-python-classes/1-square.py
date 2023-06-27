13fe646b3835f8b9e802388ea9c1c3a669258cc9
Fatima Zahra Sabri <fzsabri3@gmail.com>
2023-06-27 21:15:42 +0000

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square():
+    """A Square class"""
+    def __init__(self, size) -> None:
+        """Iniatializes an instance attribute(s)"""
+        self.__size = size
