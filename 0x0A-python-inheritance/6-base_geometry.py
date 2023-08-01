a360b67e6d313961ae71df97287a2dd06aa10f2e
Fatima Zahra Sabri <47488517+emilyjoo@users.noreply.github.com>
2023-08-01 16:24:44 +0200

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/6-base_geometry.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""6-base_geometry module"""
-
-
-class BaseGeometry:
-    """A BaseGeometry class"""
-    def area(self):
-        """a public instance method that raises an exception"""
-        raise Exception("area() is not implemented")
