089d8249627c805580d295fa090bb91f0a8a41bf
Fatima Zahra Sabri <47488517+emilyjoo@users.noreply.github.com>
2023-09-03 22:00:17 +0200

    Delete 0x03-python-data_structures directory

--- 0x03-python-data_structures/5-no_c.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-# 5-no_c.py
-
-
-def no_c(my_string):
-    """Remove c and C from a string."""
-    copy = [x for x in my_string if x != 'c' and x != 'C']
-    return ("".join(copy))
