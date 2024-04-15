03afc82d7249545a9305626be49b932b53d9328b
Fatima Zahra Sabri <47488517+emilyjoo@users.noreply.github.com>
2024-04-15 14:23:10 +0200

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-exports.converter = function (base) {
-  return function convertToBase (number) {
-    return number.toString(base);
-  };
-};
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-//  prints the number of arguments already printed and the new argument value
-let numOfArgPrinted = -1;
-exports.logMe = function (item) {
-  numOfArgPrinted++;
-  console.log(numOfArgPrinted + ': ' + item);
-};
