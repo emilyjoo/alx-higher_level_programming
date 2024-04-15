e230af35f29f351067b2be7bbfdd7021a5a0c329
Fatima Zahra Sabri <47488517+emilyjoo@users.noreply.github.com>
2024-04-15 14:22:52 +0200

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-function callMeMoby (x, callBack) {
-  for (let i = 0; i < x; i++) {
-    callBack();
-  }
-}
-
-module.exports.callMeMoby = callMeMoby;
--- 0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const arg = process.argv[2];
-
-if (arg !== undefined) {
-  console.log(arg);
-} else {
-  console.log('No argument');
-}
